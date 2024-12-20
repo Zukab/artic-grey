5afadedfbdc55b2b1a8300a9075c42ea9b8093e3
Sebastian <beerfx357@gmail.com>
2024-12-20 01:13:31 -0500

    omg finally i fix the bug, it took me like 7 hours <3 !!!

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -61,10 +61,8 @@ export function Drawer({
                 leaveFrom="translate-x-0"
                 leaveTo={offScreen[openFrom]}
               >
-                <Dialog.Panel className="w-screen max-w-lg text-left align-middle transition-all transform shadow-xl h-screen-dynamic bg-contrast">
-                  <header
-                    className={`sticky top-0 flex items-center px-6 h-nav sm:px-8 md:px-12 justify-end`}
-                  >
+                <Dialog.Panel className="w-screen max-w-lg text-left align-middle transition-all transform shadow-xl h-screen-dynamic bg-white">
+                  <header className="hidden">
                     <button
                       type="button"
                       className="p-4 -m-4 transition text-primary hover:text-primary/50"
